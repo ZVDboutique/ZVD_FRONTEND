4a1d477206ef9f18fd668555ece78a3cdcff05dc
utsavPatel <patel.utsav@icloud.com>
2025-04-23 00:54:48 +0530

    feat: restructure routing and introduce new dashboard layout
    - Replaced the About and Home pages with a new DashboardLayout component.
    - Added new routes for Dashboard, Admin Users, Company Master, Client Master, Stocks, Web Tracking, and Subscription.
    - Implemented a PrivateRoute to protect dashboard routes.
    - Created new components for Admin Users, Client Master, Company Master, Dashboard, Stocks, Subscription, and Web Tracking.
    - Removed unused About and Home pages to streamline the application.

--- src/Components/PrivateRoute.tsx
+++ src/Components/PrivateRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = () => {
   const token = localStorage.getItem("token");
 
-  if (!token) {
+  if (token) {
     return <Navigate to="/login" replace />;
   }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./Components/PrivateRoute";
-import About from "./Pages/About";
+import DashboardLayout from "./Layouts/DashboardLayout";
+import AdminUser from "./Pages/AdminUsers";
 import CompanyInformation from "./Pages/Authentication/CompanyInformation";
 import KycVerfication from "./Pages/Authentication/KycVerfication";
 import Login from "./Pages/Authentication/Login";
 import Registration from "./Pages/Authentication/Registeration";
-import Contact from "./Pages/Contact";
-import Home from "./Pages/Home";
+import ClientMaster from "./Pages/ClientMaster";
+import CompanyMaster from "./Pages/CompanyMaster";
+import Dashboard from "./Pages/Dashboard";
 import NotFound from "./Pages/NotFound";
+import Stock from "./Pages/Stocks";
+import Subscription from "./Pages/Subscription";
+import WebTracking from "./Pages/WebTracking";
 
 export const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <NotFound />,
+  },
   {
     path: "/login",
     element: <Login />,
@@ -30,16 +39,38 @@ export const router = createBrowserRouter([
     element: <PrivateRoute />,
     children: [
       {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "/stock",
+            element: <Stock />,
+          },
+
+          {
+            path: "/adminUser",
+            element: <AdminUser />,
+          },
+          {
+            path: "/company",
+            element: <CompanyMaster />,
+          },
+          {
+            path: "/clients",
+            element: <ClientMaster />,
+          },
+          {
+            path: "/web-tracking",
+            element: <WebTracking />,
+          },
+          {
+            path: "/subscription",
+            element: <Subscription />,
+          },
+        ],
       },
     ],
   },
