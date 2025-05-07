b8be5c25d8f4c0970f267639144abc06910cf92c
utsavoo7 <patel.utsav@icloud.com>
2025-05-07 20:43:57 +0530

    Completed the filtert

--- src/Pages/ExplorerDiamonds/DiamondFilterModal.tsx
+++ src/Pages/ExplorerDiamonds/DiamondFilterModal.tsx
@@ -54,11 +54,12 @@ const DiamondFilterModal = ({
     route: `/Cut`,
   });
 
-  const { data: caratWeightList, isFetching: caratWeightListLoading } =
-    useFetchQuery<{ data: any[] }>({
-      key: ["GET_ALL_PRICE"],
-      route: `/Price`,
-    });
+  const { data: priceList, isFetching: priceListLoading } = useFetchQuery<{
+    data: any[];
+  }>({
+    key: ["GET_ALL_PRICE"],
+    route: `/Price`,
+  });
 
   const { data: fluorescenceList, isFetching: fluorescenceListLoading } =
     useFetchQuery<{ data: FluorescenceInterface[] }>({
@@ -77,392 +78,140 @@ const DiamondFilterModal = ({
     clarityListLoading ||
     colorListLoading ||
     cutListLoading ||
-    caratWeightListLoading ||
+    priceListLoading ||
     fluorescenceListLoading ||
     measurementDataLoading;
 
-  console.log("🚀 ~ shapesList:", {
-    caratWeightList,
-    measurementData,
-  });
+  const accordiansData = [
+    {
+      listData: shapesList?.data,
+      name: "Shapes",
+      fieldUpdate: "shape",
+      valueField: "shape_Name",
+    },
+    {
+      listData: clarityList?.data,
+      name: "Clarity",
+      fieldUpdate: "clarity",
+      valueField: "clarity_grade",
+    },
+    {
+      listData: colorList?.data,
+      name: "Colors",
+      fieldUpdate: "color",
+      valueField: "color_grade",
+    },
+    {
+      listData: cutList?.data,
+      name: "Cut",
+      fieldUpdate: "cut",
+      valueField: "cut_Quality",
+    },
+    {
+      listData: priceList?.data,
+      name: "Price",
+      fieldUpdate: "price",
+      valueField: "price",
+    },
+    {
+      listData: fluorescenceList?.data,
+      name: "Fluorescence Intensity",
+      fieldUpdate: "fluorescenceIntensity",
+      valueField: "intensity",
+    },
+    {
+      listData: measurementData?.data,
+      name: "Width Measurement",
+      fieldUpdate: "widthMin",
+      valueField: "width",
+    },
+    {
+      listData: measurementData?.data,
+      name: "Length Measurement",
+      fieldUpdate: "lengthMin",
+      valueField: "length",
+    },
+  ];
 
   return isLoading ? (
     <Loader />
   ) : (
     <Grid container>
       <Grid size={12}>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Shapes</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {shapesList?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      shape: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.shape === data.id ? "#fff" : "#123449",
-                    bgcolor:
-                      filtersParams?.shape === data.id ? "#123449" : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.shape === data.id ? "#fff" : "#123449"
-                    }
-                  />
-                  <Typography>{data.shape_Name}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Clarity</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {clarityList?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.clarity === data.id ? "#fff" : "#123449",
-                    bgcolor:
-                      filtersParams?.clarity === data.id ? "#123449" : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.clarity === data.id ? "#fff" : "#123449"
-                    }
-                  />
-                  <Typography>{data.clarity_grade}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Cut List</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {cutList?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color: filtersParams?.cut === data.id ? "#fff" : "#123449",
-                    bgcolor:
-                      filtersParams?.cut === data.id ? "#123449" : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={filtersParams?.cut === data.id ? "#fff" : "#123449"}
-                  />
-                  <Typography>{data.cut_Quality}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Color</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {colorList?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.color === data.id ? "#fff" : "#123449",
-                    bgcolor:
-                      filtersParams?.color === data.id ? "#123449" : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.color === data.id ? "#fff" : "#123449"
-                    }
-                  />
-                  <Typography>{data.color_grade}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Fluorescence Intensity</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {fluorescenceList?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.fluorescenceIntensity === data.id
-                        ? "#fff"
-                        : "#123449",
-                    bgcolor:
-                      filtersParams?.fluorescenceIntensity === data.id
-                        ? "#123449"
-                        : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.fluorescenceIntensity === data.id
-                        ? "#fff"
-                        : "#123449"
-                    }
-                  />
-                  <Typography>{data.intensity}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Measurement</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {measurementData?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.lengthMin === data.length
-                        ? "#fff"
-                        : "#123449",
-                    bgcolor:
-                      filtersParams?.lengthMin === data.length
-                        ? "#123449"
-                        : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.lengthMin === data.length
-                        ? "#fff"
-                        : "#123449"
-                    }
-                  />
-                  <Typography>{data.length}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
-            <Typography fontWeight={600}>Width</Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              gap: 2,
-            }}
-          >
-            {measurementData?.data?.map((data, index: number) => {
-              return (
-                <Button
-                  key={index}
-                  onClick={() => {
-                    setFilterParams({
-                      ...filtersParams,
-                      clarity: data.id,
-                    });
-                  }}
-                  sx={{
-                    minWidth: 100,
-                    maxWidth: 100,
-                    minHeight: 100,
-                    borderRadius: 2,
-                    boxShadow: "0 0 6px -1px #000",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    color:
-                      filtersParams?.widthMin === data.width
-                        ? "#fff"
-                        : "#123449",
-                    bgcolor:
-                      filtersParams?.widthMin === data.width
-                        ? "#123449"
-                        : "#fff",
-                    p: 2,
-                  }}
-                >
-                  <img
-                    src="labRing.svg"
-                    alt=""
-                    color={
-                      filtersParams?.widthMin === data.width
-                        ? "#fff"
-                        : "#123449"
-                    }
-                  />
-                  <Typography>{data.width}</Typography>
-                </Button>
-              );
-            })}
-          </AccordionDetails>
-        </Accordion>
+        {accordiansData?.map((dataItem) => {
+          return (
+            <Accordion>
+              <AccordionSummary expandIcon={<MdExpandMore size={20} />}>
+                <Typography fontWeight={600}>{dataItem.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: 2,
+                }}
+              >
+                {dataItem?.listData?.map((data, index: number) => {
+                  return (
+                    <Button
+                      key={index}
+                      onClick={() => {
+                        setFilterParams({
+                          ...filtersParams,
+                          [dataItem.fieldUpdate]: (data as Record<string, any>)[
+                            dataItem.valueField
+                          ],
+                        });
+                      }}
+                      sx={{
+                        minWidth: 100,
+                        maxWidth: 100,
+                        minHeight: 100,
+                        borderRadius: 2,
+                        boxShadow: "0 0 6px -1px #000",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        color:
+                          (filtersParams as Record<string, any>)[
+                            dataItem.fieldUpdate
+                          ] ===
+                          (data as Record<string, any>)[dataItem.valueField]
+                            ? "#fff"
+                            : "#123449",
+                        bgcolor:
+                          (filtersParams as Record<string, any>)[
+                            dataItem.fieldUpdate
+                          ] ===
+                          (data as Record<string, any>)[dataItem.valueField]
+                            ? "#123449"
+                            : "#fff",
+                        p: 2,
+                      }}
+                    >
+                      <img
+                        src="labRing.svg"
+                        alt=""
+                        color={
+                          (filtersParams as Record<string, any>)[
+                            dataItem.fieldUpdate
+                          ] ===
+                          (data as Record<string, any>)[dataItem.valueField]
+                            ? "#fff"
+                            : "#123449"
+                        }
+                      />
+                      <Typography>
+                        {(data as Record<string, any>)[dataItem.valueField]}
+                      </Typography>
+                    </Button>
+                  );
+                })}
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
       </Grid>
     </Grid>
   );
