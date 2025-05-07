460b9f7398584f4fd94ea7e90f31b2af61adf865
utsavoo7 <patel.utsav@icloud.com>
2025-05-07 20:46:19 +0530

    Changes

--- src/Pages/ExplorerDiamonds/DiamondsList.tsx
+++ src/Pages/ExplorerDiamonds/DiamondsList.tsx
@@ -27,58 +27,60 @@ const DiamondsList = ({
 
   return (
     <>
-      <SimpleTable
-        columns={[
-          {
-            minWidth: 150,
-            field: "imageUrl",
-            headerName: "Image",
-            renderCell: (d) => {
-              return <img src={d.row.imageUrl || ""} />;
+      <Show.When isTrue={filtersParams.type}>
+        <SimpleTable
+          columns={[
+            {
+              minWidth: 150,
+              field: "imageUrl",
+              headerName: "Image",
+              renderCell: (d) => {
+                return <img src={d.row.imageUrl || ""} />;
+              },
             },
-          },
-          { minWidth: 250, field: "name", headerName: "Name" },
-          { minWidth: 150, field: "shapeName", headerName: "Shape" },
-          { minWidth: 150, field: "caratWeight", headerName: "Carat Weight" },
-          { minWidth: 150, field: "cutQuality", headerName: "Cut Quality" },
-          { minWidth: 150, field: "colorGrade", headerName: "Color" },
-          { minWidth: 150, field: "clarityGrade", headerName: "Clarity" },
-          {
-            minWidth: 150,
-            field: "fluorescenceIntensity",
-            headerName: "Fluorescence",
-          },
-          { minWidth: 150, field: "length", headerName: "Length" },
-          { minWidth: 150, field: "width", headerName: "Width" },
-          { minWidth: 150, field: "depth", headerName: "Dept" },
-          { minWidth: 150, field: "diameter", headerName: "Diameter" },
-          { minWidth: 150, field: "price", headerName: "Price" },
-          {
-            minWidth: 150,
-            field: "stockQuantity",
-            headerName: "Stock Quantity",
-          },
-          { minWidth: 150, field: "isAvailable", headerName: "Available" },
-          { minWidth: 150, field: "isVoid", headerName: "Void" },
-          { minWidth: 150, field: "isVerified", headerName: "Verified" },
-          { minWidth: 150, field: "isCertified", headerName: "Certified" },
-        ]}
-        rows={
-          data?.data?.map((d, i) => {
-            return { ...d, id: i };
-          }) || []
-        }
-        headerComponent={
-          <Button
-            variant="contained"
-            onClick={() => {
-              setShowFilterModal(true);
-            }}
-          >
-            Filter Diamonds
-          </Button>
-        }
-      />
+            { minWidth: 250, field: "name", headerName: "Name" },
+            { minWidth: 150, field: "shapeName", headerName: "Shape" },
+            { minWidth: 150, field: "caratWeight", headerName: "Carat Weight" },
+            { minWidth: 150, field: "cutQuality", headerName: "Cut Quality" },
+            { minWidth: 150, field: "colorGrade", headerName: "Color" },
+            { minWidth: 150, field: "clarityGrade", headerName: "Clarity" },
+            {
+              minWidth: 150,
+              field: "fluorescenceIntensity",
+              headerName: "Fluorescence",
+            },
+            { minWidth: 150, field: "length", headerName: "Length" },
+            { minWidth: 150, field: "width", headerName: "Width" },
+            { minWidth: 150, field: "depth", headerName: "Dept" },
+            { minWidth: 150, field: "diameter", headerName: "Diameter" },
+            { minWidth: 150, field: "price", headerName: "Price" },
+            {
+              minWidth: 150,
+              field: "stockQuantity",
+              headerName: "Stock Quantity",
+            },
+            { minWidth: 150, field: "isAvailable", headerName: "Available" },
+            { minWidth: 150, field: "isVoid", headerName: "Void" },
+            { minWidth: 150, field: "isVerified", headerName: "Verified" },
+            { minWidth: 150, field: "isCertified", headerName: "Certified" },
+          ]}
+          rows={
+            data?.data?.map((d, i) => {
+              return { ...d, id: i };
+            }) || []
+          }
+          headerComponent={
+            <Button
+              variant="contained"
+              onClick={() => {
+                setShowFilterModal(true);
+              }}
+            >
+              Filter Diamonds
+            </Button>
+          }
+        />
+      </Show.When>
       <Show.When isTrue={showFilterModal}>
         <SimpleSideModal
           open={showFilterModal}
